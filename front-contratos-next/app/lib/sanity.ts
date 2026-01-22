7973df535046ad894e8158da23f53e3729f46f8a
mosiah-andrade <mosiahassuncao@gmail.com>
2026-01-22 08:54:13 -0300

    fix: remover pasta api e eslint config para deploy estatico

--- front-contratos-next/app/lib/sanity.ts
+++ front-contratos-next/app/lib/sanity.ts
@@ -1,19 +1,17 @@
 // app/lib/sanity.ts
-
 import { createClient } from "next-sanity";
-import imageUrlBuilder from '@sanity/image-url';
+import createImageUrlBuilder from '@sanity/image-url'; // <-- Mudou aqui
 
 export const client = createClient({
-  projectId: "h5k6om8h", // Copie do painel da Sanity
+  projectId: "h5k6om8h", 
   dataset: "production",
   apiVersion: "2024-01-01",
-  useCdn: false, // Use 'false' para ver as atualizações em tempo real enquanto desenvolve
+  useCdn: false, 
 });
 
-// Configuração do construtor de URLs de imagem
-const builder = imageUrlBuilder(client);
+// Configuração atualizada
+const builder = createImageUrlBuilder(client);
 
-// Função auxiliar para gerar URLs de imagem nos componentes
 export function urlFor(source: any) {
   return builder.image(source);
 }
